d94638627eabd772b7ec2990991e3149d232d660
Linkunder <linkunder@hotmail.com>
2016-10-25 12:19:11 -0300

    Cambios

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-<?php
-	echo "Hola Mundo";
-	echo "how are you?";
-?>
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="UTF-8">
+	</head>
+		<body>
+			<h1>
+				<?php echo 'Hola Mundo'; ?>
+			</h1>
+			<?php
+				for ($i=2; $i <6; $i++) { 
+					echo "<h$i>¿Como estas?</h$i>\n";
+				}
+			?>
+		</body>
+</html>
